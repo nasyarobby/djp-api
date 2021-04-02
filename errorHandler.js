3460e5b997d9a9d84273fbec3e845a0b072b7665
NSRB <nasyarobby@gmail.com>
2021-04-02 14:55:13 +0700

    tambah dev_only mesage dan log unhandled error

--- errorHandler.js
+++ errorHandler.js
@@ -1,5 +1,7 @@
 const { ClientError, ServerError } = require("./utils/errors");
 
+const DEV_ONLY_MESSAGE = "DATA ONLY APPEARS ON DEV ENVIRONMENT AND WILL NOT BE AVAILABLE IN PRODUCTION ENVIRONMENT. USE ONLY FOR DEBUGGING PURPOSE ONLY."
+
 module.exports.errorHandler = (error, request, reply) => {
   if (error.validation && error.validation.length > 0) {
     const message = error.message;
@@ -7,10 +9,9 @@ module.exports.errorHandler = (error, request, reply) => {
       message,
       process.env.NODE_ENV !== "production" && error.validation.length
         ? {
-            warning:
-              "DATA ONLY APPEARS ON DEV ENVIRONMENT AND WILL NOT BE AVAILABLE IN PRODUCTION ENVIRONMENT. USE ONLY FOR DEBUGGING PURPOSE ONLY.",
-            ...error.validation[0],
-          }
+          warning: DEV_ONLY_MESSAGE,
+          ...error.validation[0],
+        }
         : undefined,
       {
         code: 400,
@@ -23,16 +24,21 @@ module.exports.errorHandler = (error, request, reply) => {
     return reply.box(
       error.message,
       process.env.NODE_ENV !== "production"
-        ? { ...error.data, stack: error.stack, error: error.error }
+        ? {
+          warning: DEV_ONLY_MESSAGE, ...error.data, stack: error.stack, error: error.error
+        }
         : error.data,
       { code: error.code, status: error.status }
     );
   }
 
+  // Unhandled error, log sebagai error.
+  request.log.error(error)
+
   return reply.box(
     "Internal server error",
     process.env.NODE_ENV !== "production"
-      ? { ...error, stack: error.stack, error: error.error }
+      ? { warning: DEV_ONLY_MESSAGE, ...error, stack: error.stack, error: error.error }
       : error.data,
     { code: 500, status: "error" }
   );
