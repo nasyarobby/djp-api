88fefac5d61f84d355abf4be1400949fe4d274ce
NSRB <nasyarobby@gmail.com>
2021-04-07 21:27:49 +0700

    fix variable name dan tambah komen

--- DjpApi.js
+++ DjpApi.js
@@ -55,11 +55,13 @@ function DJPApi(config) {
 
   app.setErrorHandler(errorHandler);
 
-  const x = Array.isArray(notFoundHandler)
+  // Jika config.notFoundHandler belum berupa array, masukkan ke dalam array
+  const arrayNotFoundHandlers = Array.isArray(notFoundHandler)
     ? notFoundHandler
     : [notFoundHandler];
 
-  const y = [
+  // default notFoundHandler -- jika user tidak menset config.notFoundHandler
+  const defaultNotFoundHandler = [
     function handler(request, reply) {
       reply
         .status(404)
@@ -67,10 +69,11 @@ function DJPApi(config) {
     },
   ];
 
+  // Jika config.notFoundHandler tidak ditemukan gunakan defaultNotFoundHandler
   app.setNotFoundHandler(
     ...(notFoundHandler
-      ? x
-      : y),
+      ? arrayNotFoundHandlers
+      : defaultNotFoundHandler),
   );
 
   return this;
