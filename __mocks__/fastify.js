b94ac7a32809104587984533430dddca9df9a067
NSRB <nasyarobby@gmail.com>
2021-04-07 23:47:41 +0700

    remove mock

--- __mocks__/fastify.js
+++
@@ -1,9 +0,0 @@
-const fastify = jest.fn().mockImplementation(() => ({
-  register: jest.fn(),
-  addHook: jest.fn(),
-  decorateReply: jest.fn(),
-  setErrorHandler: jest.fn(),
-  setNotFoundHandler: jest.fn(),
-}));
-
-module.exports = fastify;
