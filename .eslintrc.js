1a6f4442a39d06853994e97744073cf173162765
NSRB <nasyarobby@gmail.com>
2021-04-07 23:30:48 +0700

    add eslint jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,10 +3,11 @@ module.exports = {
     browser: true,
     commonjs: true,
     es2021: true,
-    "jest/globals": true,
+    'jest/globals': true,
   },
   extends: [
     'airbnb-base',
+    'plugin:jest/recommended',
   ],
   parserOptions: {
     ecmaVersion: 12,
